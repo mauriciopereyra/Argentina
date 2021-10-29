61210ce334f327794ab4185ebfe9d0ff2bc6f0c1
mauriciopereyra <pereyra.acebedo@hotmail.com>
2021-10-30 04:21:09 +0700

    Added changing banner images

--- src/Banner.js
+++ src/Banner.js
@@ -2,9 +2,28 @@ import './Banner.css'
 import logo from './logo-afa-1c.png'
 import banner from './banner.jpg'
 
+var images = [
+	"banner.jpg",
+	"banner2.jpg",
+	"banner3.jpg",
+  ]
+
+
+function changeBackground(){
+	var i = 0;
+	
+	setInterval(function() {
+		  document.getElementsByClassName('Banner')[0].style.backgroundImage = "url(" + images[i] + ")";
+		  i = i + 1;
+		  if (i == images.length) {
+			i =  0;
+		  }
+	}, 5000);
+}
+
 function Banner(){
 	return (
-		<div className="Banner">
+		<div className="Banner" onLoad={changeBackground()}>
 			<div className="Title-and-logo">
 				<div className="Title fade-in3">
 					<h2 className="fade-in">
